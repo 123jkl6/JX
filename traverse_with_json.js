25c434fb568afce7c93b1c19a531def9db443b30
123jkl6 <45234746+123jkl6@users.noreply.github.com>
2019-04-15 23:30:04 +0800

    Some more changes

--- traverse_with_json.js
+++ traverse_with_json.js
@@ -3,8 +3,11 @@ var path = require('path');
 
 var jsonFile = "/Users/wanaccess/eclipse-workspace/node_examples/sample.json";
 var directory = "";
-
 var content = "";
+var missingFiles = [];
+var keywords = [];
+var keywordsResults = [];
+
 fs.readFile(jsonFile,function(err,data){
 	if (err){
 		throw err;
@@ -17,20 +20,40 @@ fs.readFile(jsonFile,function(err,data){
 	
 	directory = obj["dir"];
 	var contentObj = obj["dirSchema"];
+	keywords = obj["keywords"];
 	
 	resolveDir(contentObj,directory);
+	
+	setTimeout(function(){
+		console.log("----------Missing Files-----------");
+		console.log(missingFiles);
+		console.log("----------Keywords-----------");
+		console.log(keywordsResults);
+	},3000);
 });
 
-function resolveDir(jsonDir,dirInput){
+async function resolveDir(jsonDir,dirInput){
 	fs.readdir(dirInput,function(err,list){
 		for (var oneFile in jsonDir){
 			if (!list.includes(oneFile)){
-				console.log(path.resolve(dirInput,oneFile));
+				//console.log(path.resolve(dirInput,oneFile));
+				missingFiles.push(path.resolve(dirInput,oneFile));
 			}
 			if ((typeof jsonDir[oneFile])=="object"){
 				resolveDir(jsonDir[oneFile],path.resolve(dirInput,oneFile));
 			}
+			else if (jsonDir[oneFile]=="file"){
+				var stringIn = "";
+				fs.readFile(path.resolve(dirInput,oneFile),function(error,dataIn){
+					stringIn += dataIn;
+					for (var oneWord of keywords){
+						if (stringIn.includes(oneWord)){
+							keywordsResults.push(oneWord+" is in "+path.resolve(dirInput,oneFile));
+						}
+					}
+				});
+			}
 		}
+		
 	});
-	
 }
