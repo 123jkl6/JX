8074ba2f1e3b0e42ddb505873b799c570c31e24d
123jkl6 <45234746+123jkl6@users.noreply.github.com>
2019-04-15 23:03:10 +0800

    "Added further requirement"

--- traverse_with_json.js
+++ traverse_with_json.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 
 var jsonFile = "/Users/wanaccess/eclipse-workspace/node_examples/sample.json";
-var directory = "/Users/wanaccess/someDir";
+var directory = "";
 
 var content = "";
 fs.readFile(jsonFile,function(err,data){
@@ -12,8 +12,11 @@ fs.readFile(jsonFile,function(err,data){
 	
 	content += data;
 	
-	var contentObj = JSON.parse(content);
-	console.log(contentObj);
+	var obj = JSON.parse(content);
+	console.log(obj);
+	
+	directory = obj["dir"];
+	var contentObj = obj["dirSchema"];
 	
 	resolveDir(contentObj,directory);
 });
@@ -25,7 +28,6 @@ function resolveDir(jsonDir,dirInput){
 				console.log(path.resolve(dirInput,oneFile));
 			}
 			if ((typeof jsonDir[oneFile])=="object"){
-				console.log(oneFile);
 				resolveDir(jsonDir[oneFile],path.resolve(dirInput,oneFile));
 			}
 		}
