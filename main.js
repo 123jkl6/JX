baa8044ebfc603fa1a8026fa9891e3c323650cc9
developer <norton.ngcx@gmail.com>
2019-05-12 22:02:21 +0800

    added base directory

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 const traverse = require("./traverse/traverse");
 
+//first input is json file path
+//next input is base directory
+//for me I use home, you can also use any other directory
 traverse("/home/developer/json_for_directory/sample.json",process.env.HOME);
